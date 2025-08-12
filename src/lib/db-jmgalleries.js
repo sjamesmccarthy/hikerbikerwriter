70e7b8ad617e83da1bf0fc69a235c94189770406
James McCarthy <james@jmgalleries.com>
2025-08-12 11:12:52 -0700

    celaning up db-pool for jmg

--- src/lib/db-jmgalleries.js
+++ src/lib/db-jmgalleries.js
@@ -1,13 +1,13 @@
 import mysql from "mysql2/promise";
 
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === "production";
 
 const jmGalleriesPool = mysql.createPool({
   host: isDev ? "127.0.0.1" : process.env.DB_HOST,
   port: isDev ? 3306 : parseInt(process.env.DB_PORT || "3306"),
   user: isDev ? "root" : process.env.DB_USER_JMG,
   password: isDev ? "root" : process.env.DB_PASSWORD_JMG,
-  database: isDev ? "jmgalusa_website" : process_env.DB_NAME_JMG,
+  database: isDev ? "jmgalusa_website" : process.env.DB_NAME_JMG,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
