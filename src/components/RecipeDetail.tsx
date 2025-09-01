47faa55776faf66811b7ece412052dd0901d6068
James McCarthy <james@jmgalleries.com>
2025-08-31 18:13:04 -0700

    upd to pdf servings

--- src/components/RecipeDetail.tsx
+++ src/components/RecipeDetail.tsx
@@ -840,7 +840,7 @@ const RecipeDetail = React.memo(function RecipeDetail({
       yPosition += descLines.length * 5 + 7;
 
       // Ingredients
-      pdf.setFontSize(14);
+      pdf.setFontSize(11);
       pdf.setFont("helvetica", "bold");
       pdf.text(`Ingredients (Servings: ${servings})`, leftMargin, yPosition);
       yPosition += 10;
